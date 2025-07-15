600c39c739cb4cb8256a4680c00f90c9badfd5c9
Meetnepali <126581718+Meetnepali@users.noreply.github.com>
2025-07-15 16:50:07 +0530

    Add app/background.py

---
+++ app/background.py
@@ -0,0 +1,3 @@
+def send_thank_you_email(name: str, email: str, product: str):
+    # Simulated async email operation, just logging
+    print(f"[Background Task] Thank you email sent to {name} <{email}> for feedback on '{product}'.")
